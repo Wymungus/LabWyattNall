7f21edeb3f795b0e75d36bf378c48913b93803eb
100153234 <100153234@lubbockisd.net>
2023-09-08 10:06:13 -0500

    Initial commit

---
+++ src/Variables.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    public class Variables
+    {
+        public static void main ( String[] args )
+        {
+//define 1 variable of each of the
+//following data types
+//byte short int long
+//float double
+//char boolean String
+//integer variables
+            byte byteOne = 127;
+            System.out.println(&quot;/////////////////////////////////&quot;);
+            System.out.println(&quot;*Some Person 07/18/02*&quot;);
+            System.out.println(&quot;* *&quot;);
+            System.out.println(&quot;* integer types *&quot;);
+            System.out.println(&quot;* *&quot;);
+            System.out.println(&quot;*8 bit - byteOne = &quot;+byteOne+&quot;\t\t*&quot;);
+        }
+    }
+
+}
